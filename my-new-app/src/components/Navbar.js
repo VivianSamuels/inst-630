dc819a80916038fff03aef7870dde6609bac3380
VivianSamuels <vsamuels@umd.edu>
2022-11-07 15:38:46 -0500

    made the nav bar items horizontal

--- my-new-app/src/components/Navbar.js
+++ my-new-app/src/components/Navbar.js
@@ -7,13 +7,11 @@ function Navbar() {
         <div class="nav-container">
             <nav>
                 <div id="logo"><img src={Logo} alt="Star Wars Logo"/> </div>
-                <div>
                     <ul class ="nav-buttons">
                         <li>The Phantom Menance</li>
                         <li>Attack of the Clones</li>
                         <li>Revenger of the Sith</li>
                     </ul>
-                </div>
             </nav>
         </div>
     )
