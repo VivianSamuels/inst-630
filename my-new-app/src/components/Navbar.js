b048f22cbd7eb3dcaafc07152a06cd94a7d460e7
VivianSamuels <vsamuels@umd.edu>
2022-11-07 14:55:36 -0500

    added logo, nav buttons, and light styling

--- my-new-app/src/components/Navbar.js
+++ my-new-app/src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Logo from '../images/starWarsLogo.png'
 import './Navbar.css'
 
 function Navbar() {
     return (
         <div class="nav-container">
             <nav>
-                
+                <div id="logo"><img src={Logo}/> </div>
+                <div>
+                    <ul class ="nav-buttons">
+                        <li>The Phantom Menance</li>
+                        <li>Attack of the Clones</li>
+                        <li>Revenger of the Sith</li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )
