fedc718629d23e2e244f95f854466d2418b3a2a4
VivianSamuels <vsamuels@umd.edu>
2022-11-07 16:28:46 -0500

    added content and style to main content

--- my-new-app/src/components/Maincontent.js
+++ my-new-app/src/components/Maincontent.js
@@ -1,6 +1,9 @@
 import React from "react";
 import './Maincontent.css';
-import Background from '../images/space.png'
+import Background from '../images/space.png';
+import Clones from '../images/AttackClones.jpg';
+import Phantom from '../images/PhantomMenance.jpg';
+import Sith from '../images/RevengeSith.jpg';
 
 function Maincontent {
     return (
@@ -8,15 +11,15 @@ function Maincontent {
             <div id="bg-image"><img src={Background} alt="an image of stars in a night sky"/></div>
 
             <div className="movie1">
-                
+                <img src={Phantom} alt="Phantom Menance movie poster"/>
             </div>
 
             <div className="movie2">
-
+                <img src={Clones} alt="Attack of the Clones movie poster"/>
             </div>
             
             <div className="movie3">
-
+                <img src={Sith} alt="Revenge of the Sith movie poster"/>
             </div>
         </div>
 
