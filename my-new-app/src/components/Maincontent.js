6b99fd597c2ffecb0505cd34aac33f7c5f72a4b3
VivianSamuels <vsamuels@umd.edu>
2022-11-07 16:13:20 -0500

    minor naming edits

--- my-new-app/src/components/Maincontent.js
+++ my-new-app/src/components/Maincontent.js
@@ -1,2 +1,26 @@
 import React from "react";
 import './Maincontent.css';
+import Background from '../images/space.png'
+
+function Maincontent {
+    return (
+        <div className="main-container">
+            <div id="bg-image"><img src={Background} alt="an image of stars in a night sky"/></div>
+
+            <div className="movie1">
+                
+            </div>
+
+            <div className="movie2">
+
+            </div>
+            
+            <div className="movie3">
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Maincontent;
