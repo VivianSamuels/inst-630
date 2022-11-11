d4c9d42af2b551307e9f51417de125e87d68abbd
VivianSamuels <vsamuels@umd.edu>
2022-11-11 12:09:20 -0500

    added script data and framework for first page

---
+++ my-new-app/src/components/Menance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Data from '../data/script-data.json';
+import {LineChart, Line} from 'recharts';
+
+const data = 
+
+function Menance() {
+    return(
+
+    )
+}
+export default Menance
