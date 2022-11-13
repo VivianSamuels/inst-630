c5bbaaeb85fa48eb9d4aaf5e1b739a946809ccf5
VivianSamuels <vsamuels@umd.edu>
2022-11-13 18:38:53 -0500

    trying to get the data to work

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Maincontent from './components/Maincontent'
+import Menace from './components/Menace'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Maincontent />
+      <Menace />
     </div>
 
   );
--- my-new-app/src/components/Menace.js
+++ my-new-app/src/components/Menace.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Data from '../data/script-data.json';
-import {LineChart, Line} from 'recharts';
+// import {LineChart, Line} from 'recharts';
 
 const rawData = Data;
 
-let epiOne [];
+let epiOne = [];
+epiOne.push(rawData);
 
 
 function Menance() {
     return(
-
+       <div className="m-wrapper">
+            console.log(epiOne)
+       </div>
     )
 }
 export default Menance
