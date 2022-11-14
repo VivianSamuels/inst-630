5a905c612eea530c7e7f3fe2474d1b043e9d76c0
VivianSamuels <vsamuels@umd.edu>
2022-11-14 16:37:11 -0500

    added JSON data

--- my-new-app/src/components/Menace.js
+++ my-new-app/src/components/Menace.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import Data from '../data/script-data.json';
-// import {LineChart, Line} from 'recharts';
-
-const rawData = Data;
-
-let epiOne = [];
-epiOne.push(rawData);
+import React from 'react'
+import Data from '../data/script-data.json'
+import { LineChart, Line, BarChart, XAxis } from 'recharts'
 
 
 function Menance() {
+    const rawData = Data.scripts;
+    //console.log(rawData[0].type);
+    let epiOne = [];
+    for (let i = 0; i<Object.keys(rawData).length; i++) {
+        if (rawData[i].episode === 1 & rawData[i].type === "dialogue"){
+            epiOne.push(rawData[i]);
+        }
+    }
+    console.log(epiOne[0].line.length);
+    console.log(epiOne[0].line.split(" "));
+
+
+
     return(
        <div className="m-wrapper">
-            console.log(epiOne)
+        {/* <BarChart width={100%} aspect={3} data={rawData}>
+            <XAxis datakey="character"/>
+        </BarChart> */}
        </div>
     )
 }
