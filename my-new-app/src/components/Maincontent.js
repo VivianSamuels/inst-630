4fdd584097615cdbe70595269e59b83b17f7520f
VivianSamuels <vsamuels@umd.edu>
2022-11-19 13:56:49 -0500

    added a margin for the graph won't overlap /ns

--- my-new-app/src/components/Maincontent.js
+++ my-new-app/src/components/Maincontent.js
@@ -9,18 +9,21 @@ function Maincontent () {
     return (
         <div className="main-container">
             <div id="background"><img src={Background} alt="stars in a night sky" id="bg-image"/></div>
-
-            <div className="movie1">
-                <img src={Phantom} alt="Phantom Menance movie poster"/>
-            </div>
-
-            <div className="movie2">
-                <img src={Clones} alt="Attack of the Clones movie poster"/>
-            </div>
-            
-            <div className="movie3">
-                <img src={Sith} alt="Revenge of the Sith movie poster"/>
-            </div>
+            <div id="movies">
+                    <ul className ="movie-buttons">
+                        <li><img src={Phantom} alt="Phantom Menace movie poster"/>
+                        <a href = "#"><button type="button">The Phantom Menace</button></a>
+                            </li>
+                        <li><img src={Clones} alt="Attack of the Clones movie poster"/>
+                        <a href = "#">
+                            <button type="button">Attack of the Clones</button></a>
+                            </li>
+                        <li><img src={Sith} alt="Revenge of the Sith movie poster"/>
+                        <a href = "#">
+                            <button type="button">Revenge of the Sith</button></a>
+                            </li>
+                    </ul>
+                    </div>
         </div>
 
     )
