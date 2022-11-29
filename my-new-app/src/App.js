8866325e6c0f6faa611aa4e921396d11f793e4f6
VivianSamuels <vsamuels@umd.edu>
2022-11-28 19:55:29 -0500

    made data export from App.js

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,19 +1,45 @@
 import React from 'react'
+import Data from './data/script-data.json'
 import Navbar from './components/Navbar'
 import Maincontent from './components/Maincontent'
 import Prequels from './components/Prequels'
 import Force from './components/Force'
 
+function getData() {
+
+  const rawData = Data.scripts;
+  const allData = rawData.filter(item => item.episode <= 3 && item.type === "dialogue");
+
+  return allData;
+}
+
+function cleanData() {
+  let cleanData =[];
+  const prequelData = getData();
+  const freshData = prequelData.map(data => {
+    if (data.line.includes("(")) {
+        data.line = data.line.replace(/ *\([^)]*\) */g, "");
+        data.line = data.line.split(" ");
+        cleanData.push(data);
+    } else { data.line = data.line.split(" ");
+      cleanData.push(data)}
+
+    //return data;
+})
+
+return cleanData;
+}
+
 function App() {
-  return (
+
+   return (
     <div className="App">
       <Navbar />
       <Maincontent />
       <Prequels />
-      <Force />
     </div>
 
   );
 }
 
-export default App;
+export { App as default, cleanData}
