8053341f77803b8bb2afafe9471398da31f3017e
VivianSamuels <vsamuels@umd.edu>
2022-12-12 12:37:04 -0500

    Added image, cleaned mainpage

--- my-new-app/src/components/Maincontent.js
+++ my-new-app/src/components/Maincontent.js
@@ -4,18 +4,18 @@ import Background from '../images/space.png';
 import Clones from '../images/AttackClones.jpg';
 import Phantom from '../images/PhantomMenance.jpg';
 import Sith from '../images/RevengeSith.jpg';
+import Padme from '../images/Padme.png'
 
 function Maincontent () {
     return (
         <div className="main-container">
-            <div id="background"><img src={Background} alt="stars in a night sky" id="bg-image"/></div>
-            <div id="movies">
-                    {/* <ul className ="movie-buttons">
-                        <li><img src={Phantom} alt="Phantom Menace movie poster"/></li>
-                        <li><img src={Clones} alt="Attack of the Clones movie poster"/></li>
-                        <li><img src={Sith} alt="Revenge of the Sith movie poster"/></li>
-                    </ul> */}
-                    </div>
+            <div className="main-info">
+            <h1>Who is Padme?</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            <div className="padme">
+            <img src={Padme} alt="Padme Amidala looking apprehensively at something out of frame"/>    
+            </div>
         </div>
 
     )
